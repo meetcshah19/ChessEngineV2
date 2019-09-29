8d9aa92fab995ca9fe7cea217078eb63aa69c449
meetcshah19 <meetcshah19@gmail.com>
2019-09-29 08:47:50 +0530

    DBConnection updated

--- src/com/util/DBConnection.java
+++ src/com/util/DBConnection.java
@@ -3,9 +3,9 @@ import java.sql.*;
 
 public class DBConnection {
 	static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
-	static final String DB_URL = "jdbc:mysql://localhost:3306/account";
-	static final String USER = "root";
-	static final String PASS = "root";
+	static final String DB_URL = "jdbc:mysql://51.158.118.84:33060/account";
+	static final String USER = "meet";
+	static final String PASS = "meet";
 
 	public static java.sql.Connection getConnection() {
 
